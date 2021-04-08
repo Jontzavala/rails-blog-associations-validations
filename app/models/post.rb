863e887b8d92a64d2512ade0611944108474e658
Jonathan Zavala <jontzavala@gmail.com>
2021-04-08 13:41:54 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ActiveRecord::Base
-  
+    validates_presence_of :name, :content
+
+    belongs_to :user
+    has_many :post_tags
+    has_many :tags, :through => :post_tags
+    accepts_nested_attributes_for :tags
+
+    def tags_attributes=(tag_attributes)
+        tag_attributes.values.each do |tag_attribute|
+          tag = Tag.find_or_create_by(tag_attribute)
+          self.tags << tag
+        end
+    end
 end
